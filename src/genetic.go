19b9ed8994aad21fbe5b610de6375d2e49d9f7f1
Eduardo Savian <eduardosavian@outlook.com>
2024-06-01 15:04:12 -0300

    Refactoring

--- src/genetic.go
+++ src/genetic.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 )
 
+
+
 // Genetic algorithm function to calculate the best route and distance
-func geneticAlgorithm(matrix [][]int, nGenerations int, populationSize int) ([]int, int) {
+func geneticAlgorithm(matrix [][]int, numGenerations int, populationSize int) ([]int, int) {
 	printMatrix(matrix)
-	fmt.Println("Number of generations:", nGenerations)
+	fmt.Println("Number of generations:", numGenerations)
 	fmt.Println("Population size:", populationSize)
 	numCities := len(matrix)
 
-	fmt.Println(generateRandomRoute(numCities))
 
 	bestRoute := make([]int, numCities)
 	bestDistance := 0
--- src/utils.go
+++ src/utils.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"math"
-	"math/rand"
 	"os"
 	"strconv"
 	"strings"
@@ -85,6 +83,7 @@ func formatMatrix(data []int, size int) [][]int {
 
 // Print Matrix function
 func printMatrix(matrix [][]int) {
+	fmt.Println("Matrix")
 	for _, array := range matrix {
 		for j := range array {
 			fmt.Print(array[j], " ")
@@ -93,39 +92,3 @@ func printMatrix(matrix [][]int) {
 	}
 }
 
-func contains(route []int, city int) bool {
-	for _, c := range route {
-		if c == city {
-			return true
-		}
-	}
-	return false
-}
-
-func generateRandomRoute(numCities int) []int {
-	route := make([]int, numCities)
-	for i := range route {
-		route[i] = i
-	}
-	rand.Shuffle(len(route), func(i, j int) { route[i], route[j] = route[j], route[i] })
-	return route
-}
-
-func distance(matrix [][]int, city1 int, city2 int) int {
-	if city1 == city2 {
-		return 0
-	}
-	if matrix[city1][city2] == -1 {
-		return math.MaxInt32
-	}
-	return matrix[city1][city2]
-}
-
-func totalDistance(matrix [][]int, route []int) int {
-	total := 0
-	for i := 0; i < len(route)-1; i++ {
-		total += distance(matrix, route[i], route[i+1])
-	}
-	total += distance(matrix, route[len(route)-1], route[0])
-	return total
-}
